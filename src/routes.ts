20ea9f694e8dea685cbd05dcc8d2adf72221613d
Klismann Barros <klismannbarros@eng.ci.ufpb.br>
2021-02-26 17:10:17 -0300

    Finalizada a NLW04

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.get("/users", )
@@ -17,5 +21,8 @@ router.get('/surveys', surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
 
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
